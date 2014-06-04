07eca33414eba066b8720fba452443491f9fa695
Kazutaka Ashizawa <k.ashizawa@gmail.com>
2014-06-04 11:31:04 +0900

    add template method.

--- app/main.js
+++ app/main.js
@@ -1,30 +1,61 @@
 /*jshint strict:false, browser:true */
-(function bookmarklet() {
-  var styleNode = document.createElement('style'),
-    content = document.createTextNode('body { background: cornflowerblue; }');
+(function() {
+  var bookmarklet = {
 
-  styleNode.appendChild(content);
-  document.head.appendChild(styleNode);
+    myName: 'bookmarklet-alt-check',
 
-  // add
-  var myName = 'bookmarklet-alt-check',
-    version = '0.0.1',
-    url = 'https://github.com/ashizawa/bookmarklet-alt-check';
+    version: '0.0.1',
 
-  if (document.getElementById(myName)) {
-    return;
-  }
+    url: 'https://github.com/ashizawa/bookmarklet-alt-check',
 
-  var wrapper = document.createElement('div');
-  var html = '<div style="text-align:right;"><a href="' + url + '">' + myName + ' v' + version + '</a></div>';
+    /**
+     * template code output
+     * by http://krasimirtsonev.com/blog/article/Javascript-template-engine-in-just-20-line
+     *
+     * @param html - template
+     * @param options - variable
+     * @return {String} - html object
+     */
+    templateCompile: function(html, options) {
+      var re = /<%([^%>]+)?%>/g, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
+      var add = function(line, js) {
+        js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+          (code += line !== '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+        return add;
+      };
+      var match;
+      while (match = re.exec(html)) {
+        add(html.slice(cursor, match.index))(match[1], true);
+        cursor = match.index + match[0].length;
+      }
+      add(html.substr(cursor, html.length - cursor));
+      code += 'return r.join("");';
+      return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+    },
 
-  getElements('body')[0].appendChild(wrapper);
-  wrapper.innerHTML = html;
+    styleNode: document.createElement('style'),
 
-  wrapper.onclick = function() {
-    this.parentNode.removeChild(this);
+    content: document.createTextNode('body { background: #fff; }'),
+
+    initialize: function() {
+      if (document.getElementById(bookmarklet.myName)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+
+      this.styleNode.appendChild(this.content);
+      document.head.appendChild(this.styleNode);
+
+      var wrapper = document.createElement('div');
+      var html = this.templateCompile('<div style="text-align:right;"><a href="<%this.url%>"><%this.name%> v<%this.version%></a></div>', {url: this.url, name: this.myName, version: this.version});
+      document.getElementsByTagName('body')[0].appendChild(wrapper);
+      wrapper.innerHTML = html;
+      wrapper.onclick = function() {
+        this.parentNode.removeChild(this);
+      };
+    }
   };
 
-  window.scrollTo(0, 0);
+  bookmarklet.initialize();
 
-}());
+})(window);
